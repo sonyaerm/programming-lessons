b2ed37b02d8941c47e34d9e7434ca1fc3e985eec
sonyaerm <sofya-ermolova@mail.ru>
2022-03-03 16:02:00 +0300

    добавление системы логирования

--- veb.js
+++ veb.js
@@ -1,19 +1,24 @@
 const http = require("http");
+const fs = require("fs");
+
 const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
-if(request.url === "/home" || request.url === "/"){
+    if(request.url === "/home" || request.url === "/"){
+    fs.appendFileSync("hello.txt", "/home");
     response.write("<h2>Home</h2>");
     }
     else if(request.url == "/about"){
     response.write("<h2>About</h2>");
+    fs.appendFileSync("hello.txt", "/about");
     }
     else if(request.url == "/contact"){
     response.write("<h2>Contacts</h2>");
+    fs.appendFileSync("hello.txt", "/contact");
     }
     else{
     response.write("<h2>Not found</h2>");
+    fs.appendFileSync("hello.txt", "Not found");
     }
     response.end();
-    };
-    
+};
 http.createServer(requestHandler).listen(3000);
