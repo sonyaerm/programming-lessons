9b285ea739673dbaab1f916fef6d823f961fc9f4
sonyaerm <sofya-ermolova@mail.ru>
2022-03-03 10:30:50 +0300

    Маршрутизация

--- veb.js
+++ veb.js
@@ -1,5 +1,19 @@
 const http = require("http");
 const requestHandler = (request, response) => {
-response.end("Hello world!");
-};
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+if(request.url === "/home" || request.url === "/"){
+    response.write("<h2>Home</h2>");
+    }
+    else if(request.url == "/about"){
+    response.write("<h2>About</h2>");
+    }
+    else if(request.url == "/contact"){
+    response.write("<h2>Contacts</h2>");
+    }
+    else{
+    response.write("<h2>Not found</h2>");
+    }
+    response.end();
+    };
+    
 http.createServer(requestHandler).listen(3000);
